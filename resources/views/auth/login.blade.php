ba2eda2eb7991c692e69f4b359b857ee034db5a1
Neri Junior <neri@nerijunior.com>
2016-06-10 16:53:15 -0300

    fix typo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -54,7 +54,7 @@
                                     <i class="fa fa-btn fa-sign-in"></i> Login
                                 </button>
 
-                                <a class="btn btn-link" href="{{ url('/password/reset') }}">Esqueceu sua senha??</a>
+                                <a class="btn btn-link" href="{{ url('/password/reset') }}">Esqueceu sua senha?</a>
                             </div>
                         </div>
                     </form>
