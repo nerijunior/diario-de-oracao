011c443cc8b9d7d935d0e977f8c30a0e56418f13
Neri Junior <neri@nerijunior.com>
2016-06-10 03:50:07 -0300

    fix shared if

--- resources/views/diaries/list.blade.php
+++ resources/views/diaries/list.blade.php
@@ -1,7 +1,7 @@
 <ul class="posts">
     @foreach ($posts as $post)
         <li>
-            @if($shared)
+            @if(isset($shared) && $shared)
             <a href="{{ route('shared.posts.see', ['id' => $post->id]) }}">
                 {{ date('d/m/Y', strtotime($post->date)) }} - {{ $post->questions['bible_readed'] }}
             </a>
